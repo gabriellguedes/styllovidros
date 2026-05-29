51f1dc9b9d017c665740acde6007faad7630cd50
gabriellguedes <gguedes10@gmail.com>
2026-05-29 07:57:40 -0300

    Atualização e instalação do Coudinary.

--- engine/settings.py
+++ engine/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'engine.core',
+     'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -140,6 +143,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Configuração de arquivos de mídia (Fotos e Vídeos)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Permite que o site use frames do mesmo domínio
 X_FRAME_OPTIONS = 'SAMEORIGIN'
